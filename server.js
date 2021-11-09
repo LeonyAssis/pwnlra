11ab12468461652f91d72da3c3bc18434fc9bfb3
Leony Assis <leonyra@hotmail.com>
2021-11-08 22:21:04 -0300

    feat: Commit inicial

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ const http = require ('http');
 const port = process.env.PORT || '3000';
 
 const server = http.createServer((req, res) =>{
-    res.end('Euzin').writeHead(200);
+    res.end('Leony Rodrigues de Assis').writeHead(200);
 });
 
 
 server.listen(port, () =>{
-    console.log(`server running port ${port}`)
-    
+    console.log(`server running port ${port}`)    
 });
